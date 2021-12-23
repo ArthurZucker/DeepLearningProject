b9bb60268dc22d9d74fbc3bd5423282861a34930
Arthur Zucker <arthur.zucker@gmail.com>
2021-12-23 09:03:07 +0100

    added hparams for bralow twins

--- config/hparams.py
+++ config/hparams.py
@@ -15,4 +15,9 @@ Most notably, the agent, dataset, optimizer and loss can all be specified and au
 @dataclass
 class hparams:
     """Hyperparameters of Yout Model"""
-    
+    # learning rate 
+    lr : float = 0.001
+    # lambda coefficient used for FIXME
+    lmbda: float = 0.1
+    # number of channels to use for projection
+    bt_proj_channels: int = 64 # TODO run a sweep for that?
