9495eac3b6654e8ea11b1cf21bd46a92e186876c
Arthur Zucker <arthur.zucker@gmail.com>
2021-12-27 21:15:52 +0100

    BarlowTwins CIFAR10 dataset module

---
+++ dataset/BarlowTwinsDataset.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+import torch
+
+#torch dataset library
+from torch.utils.data import Dataset
+from utils.transforms import BarlowTwinsTransform
+from torchvision.datasets import CIFAR10
+from PIL import Image
+
+class BarlowTwinsDataset(CIFAR10):
+    def __init__(self, root, img_size, train,download = False):
+        super().__init__(root=root,train = train, download = False)
+        self.transform = BarlowTwinsTransform(img_size)
+
+    def __getitem__(self, idx):
+        image = Image.fromarray(self.data[idx])
+        image = np.array(image)
+        # Transform the same image with 2 different transforms
+        if self.transform is not None:
+            aug_image1, aug_image2 = self.transform(image = image)
+        return aug_image1, aug_image2    
+    
