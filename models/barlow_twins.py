04a7f2a20d5588216e4a5ac36f22c4b8400ba7bf
Arthur Zucker <arthur.zucker@gmail.com>
2021-12-27 21:26:12 +0100

    Barlow twins model updated

--- models/barlow_twins.py
+++ models/barlow_twins.py
@@ -9,12 +9,15 @@ from pytorch_lightning import LightningModule
 from torch.nn import functional as F
 from torch.optim import Adam
 
+from models.losses.barlow_twins import BarlowTwinsLoss
+from utils.agent_utils import get_net
 
 class Barlow_Twins(LightningModule):
     def __init__(self, config, encoder):
         """method used to define our model parameters"""
         super().__init__()
 
+        self.loss = BarlowTwinsLoss
         # optimizer parameters
         self.lr = config.lr
 
@@ -25,21 +28,12 @@ class Barlow_Twins(LightningModule):
         self.lmbda = config.lmbda
 
         # get backbone model and adapt it to the task
-        self.in_features = 0
-        self.encoder = encoder  # TODO add encoder name to the hparams, use getnet() to get the encoder
-        try:
-            self.in_features = list(self.encoder.children())[-1].in_features
-            last_layer_name = list(self.encoder.named_children())[-1][0]    
-            
-            #TODO encoder might not always have those layers
-            if last_layer_name == "head":
-                self.encoder.head = nn.Identity()
-            else:
-                self.encoder.fc = nn.Identity()
-        except:
-            print(
-                "Encoder should be a torchvision resnet model or timm's VIT"
-            )  # TODO Basic VIT will probably be abit too big, we might want to use SWIN
+        self.encoder = get_net(
+            config, config.encoder
+        )  # TODO add encoder name to the hparams, use getnet() to get the encoder
+        self.in_features = list(self.encoder.children())[-1].in_features
+        name_classif = list(self.encoder.named_children())[-1][0]
+        self.encoder._modules[name_classif] = nn.Identity()
 
         # Make Projector (3-layers)
         self.proj_channels = config.bt_proj_channels
@@ -72,7 +66,7 @@ class Barlow_Twins(LightningModule):
         loss = self._get_loss(batch)
 
         # Log loss and metric
-        self.log("train_loss", loss)
+        self.log("train/loss", loss)
 
         return loss
 
@@ -81,16 +75,10 @@ class Barlow_Twins(LightningModule):
         loss = self._get_loss(batch)
 
         # Log loss and metric
-        self.log("val_loss", loss)
+        self.log("val/loss", loss)
 
         return loss
 
-    def test_step(self, batch, batch_idx):
-        """used for logging metrics"""
-        loss = self._get_loss(batch)
-
-        # Log loss and metric
-        self.log("test_loss", loss)
 
     def configure_optimizers(self):
         """defines model optimizer"""
@@ -101,28 +89,6 @@ class Barlow_Twins(LightningModule):
         x1, x2 = batch
         z1, z2 = self(x1, x2)
 
-        loss = bt_loss(z1, z2, self.lmbda)
+        loss = self.loss(z1, z2, self.lmbda)
 
         return loss
-
-# TODO put the loss in either model/loss or create a folder in utils or a folder in the repo but it might be an over kill
-def bt_loss(z1, z2, lmbda):
-
-    # Normalize the projector's output across the batch
-    norm_z1 = (z1 - z1.mean(0)) / z1.std(0)
-    norm_z2 = (z2 - z2.mean(0)) / z2.std(0)
-
-    # Cross correlation matrix
-    batch_size = z1.size(0)
-    cc_M = torch.einsum("bi,bj->ij", (norm_z1, norm_z2)) / batch_size
-
-    # Invariance loss
-    diag = torch.diagonal(cc_M)
-    invariance_loss = ((torch.ones_like(diag) - diag) ** 2).sum()
-
-    # Zero out the diag elements and flatten the matrix to compute the loss
-    cc_M.fill_diagonal_(0)
-    redundancy_loss = (cc_M.flatten() ** 2).sum()
-    loss = invariance_loss + lmbda * redundancy_loss
-
-    return loss
