1aa684b3486d60069824091a96b8aa027d5ad619
Arthur Zucker <arthur.zucker@gmail.com>
2021-12-29 17:30:55 +0100

    new structure

--- config/hparams.py
+++ config/hparams.py
@@ -1,12 +1,15 @@
 from dataclasses import dataclass
 from typing import List, ClassVar, Optional
 import numpy as np
-from simple_parsing.helpers import list_field
-import random 
+from simple_parsing.helpers import list_field, choice
+import random
 import torch
+import torch.optim
+
 import simple_parsing
 import os
 from os import path as osp
+
 """Dataclass allows to have arguments and easily use wandb's weep module.
 
 Each arguments can be preceded by a comment, which will describe it when you call 'main.pu --help
@@ -18,119 +21,90 @@ Most notably, the agent, dataset, optimizer and loss can all be specified and au
 @dataclass
 class Hparams:
     """Hyperparameters of Your Model"""
-    # data directory
-    save_dir: str = osp.join(os.getcwd(), "wandb")
-    # number of channels to use for projection
-    bt_proj_channels: int = 2048 # TODO run a sweep for that?
-    # backbone to use. Should match a specific format to be defined later
-    encoder: str = "resnet50"
-    #method 
-    method : str= "barlow"
-    # seed for the whole run
-    seed_everything: Optional[int] = None
-    # project  
-    wandb_project: str = "test-deep-learning"
-    # wandb entity
-    wandb_entity: str = "dinow-twins"
-    # resize coefficients for H and w
-    input_size: tuple = (32, 32)
-    # learning rate
-    lr: float = 0.4
-    # agent to use for training
-    agent: str = "trainer"
-    # architecture to use
-    arch: str = "BarlowTwins"
-    # encoder for barlow
-    encoder: str = "resnet50"
-    # data module
-    datamodule: str = "BarlowTwinsCIFAR10DataModule"
-    # number of workers for dataloaders
-    num_workers: int = 20
-    # tune the model on first run
-    tune_lr: bool = False
-    # tune the model on first run
-    tune_batch_size: bool = False
-    # number or gpu
-    gpu: int = 1
-    # precision
-    precision: int = 32
-    # validation frequency
-    val_freq: int = 10
-    # developpment mode, only run 1 batch of train val and test
-    dev_run: bool = False
-    # gradient accumulation batch size
-    accumulate_size: int = 512
-    # batch size for training
-    batch_size: int = 2048
+    wandb_project: str = "test-deep-learning"  # name of the project
+    wandb_entity: str = "dinow-twins"  # name of the wandb entity, here our team
+    save_dir: str = osp.join(os.getcwd(), "wandb")  # directory to save wandb outputs
+    arch: str = choice(
+        "BarlowTwins", "Dino", "DinowTwin", default="BarlowTwins"
+    )  # training method, either Barlow, Dino, or DinowTwin
+    agent: str = "trainer"  # agent to use for training
+    seed_everything: Optional[int] = None  # seed for the whole run
+    input_size: tuple = (32, 32)  # resize coefficients (H,W) for classic transforms
+    tune_lr: bool = False  # tune the model on first run
+    tune_batch_size: bool = False  # tune the model on first run
+    gpu: int = 1  # number or gpu
+    precision: int = 32  # precision
+    val_freq: int = 1  # validation frequency
+    dev_run: bool = False  # developpment mode, only run 1 batch of train val and test
+    accumulate_size: int = 2048  # gradient accumulation batch size
     # maximum number of epochs
-    max_epochs: int = 40
-
-    #### TODO imperatively solve the hparams config args
-    # Image size, assumes square images
-    # img_size: int = 32
-    # number of crops/global_crops
-    n_crops: int = 8
-    # number of global crops 
-    n_global_crops: int = 2
-    # scale range of the global crops 
-    global_crops_scale: List[int]   = list_field(0.5,1)
-    local_crops_scale:  List[float] = list_field(0.08, 0.5)
-    # lambda coefficient used for FIXME ??????????
-    lmbda: float = 0.05
+    max_epochs: int = 200
     # path to download pascal voc
     asset_path: str = osp.join(os.getcwd(), "assets")
-    #log_pred_freq
+    # log_pred_freq
     log_pred_freq: int = 10
+    # log cc_M matrix frequency
+    log_ccM_freq: int = 1
+
+
 @dataclass
 class DatasetParams:
     """Dataset Parameters"""
 
-    default_root: ClassVar[str] = "/dataset"  # the default root directory to use.
-
-    dataset: str = "CIFAR10"  # laptop,pistol
-    """ dataset name: only [cifar10] for now """
-
-    root_dir: str = default_root  # dataset root directory
+    # Image size, assumes square images
+    num_workers: int = 20  # number of workers for dataloadersint
+    input_size: tuple = (32, 32)  # image_size
+    batch_size: int = 2048  # batch_size
+    asset_path: str = osp.join(os.getcwd(), "assets")  # path to download the dataset
 
 
 @dataclass
 class OptimizerParams:
     """Optimization parameters"""
 
-    optimizer: str = "adam"  # Optimizer (adam, rmsprop)
-    lr: float = 0.0001  # learning rate, default=0.0002
+    optimizer: str = "Adam"  # Optimizer (adam, rmsprop)
+    lr: float = 3e-4  # learning rate, default=0.0002
     lr_sched_type: str = "step"  # Learning rate scheduler type.
     z_lr_sched_step: int = 100000  # Learning rate schedule for z.
     lr_iter: int = 10000  # Learning rate operation iterations
     normal_lr_sched_step: int = 100000  # Learning rate schedule for normal.
-    beta1: float = 0.0  # beta1 for adam. default=0.5
-    batchSize: int = 4  # input batch size
+    betas: List[float] = list_field(0.9, 0.999)  # beta1 for adam. default=(0.9, 0.999)
 
-        
+
 @dataclass
-class BarlwoConfig:
+class BarlowConfig:
     """Hyperparameters specific to Barlow Twin Model.
     Used when the `arch` option is set to "Barlow" in the hparams
     """
-    # Image size, assumes square images
-    img_size: int = 32
-    # number of crops/global_crops
-    n_crops: int = 8
-    # number of global crops 
-    n_global_crops: int = 2
-    # scale range of the global crops 
-    global_crops_scale: List[int]   = list_field(0.5,1)
-    local_crops_scale:  List[float] = list_field(0.08, 0.5)
-    # lambda coefficient used for FIXME ??????????
-    lmbda: float = 0.05
+
+    # number of channels to use for projection
+    bt_proj_channels: int = 2048  # TODO run a sweep for that?
+    # encoder for barlow
+    encoder: str = choice("resnet50", "swinS", default="resnet50")
+    # lambda coefficient used to scale the scale of the redundancy loss
+    # so it doesn't overwhelm the invariance loss
+    lmbda: float = 5e-3
+    # datamodule to use, for now we only have one dataset, CIFAR10
+    datamodule: str = choice(
+        "BarlowTwinsCIFAR10DataModule",
+        default="BarlowTwinsCIFAR10DataModule",
+    )
+    pretrained_encoder: bool = False
+
 
 @dataclass
 class DinoConfig:
     """Hyperparameters specific to the DINO Model.
     Used when the `arch` option is set to "Barlow" in the hparams
     """
-    pass
 
+    # number of crops/global_crops
+    n_crops: int = 8
+    # number of global crops
+    n_global_crops: int = 2
+    # scale range of the crops
+    global_crops_scale: List[int] = list_field(0.5, 1)
+    local_crops_scale: List[float] = list_field(0.08, 0.5)
 
 
 @dataclass
@@ -143,6 +117,7 @@ class Parameters:
     # optimizer: OptimizerParams = OptimizerParams()
     # GAN Settings
     hparams: Hparams = Hparams()
+    optim_param: OptimizerParams = OptimizerParams() 
 
     def __post_init__(self):
         """Post-initialization code"""
@@ -150,12 +125,16 @@ class Parameters:
         # since we will use different models, backbones and datamodules
 
         # Set render number of channels
-        if self.hparams.method == "barlow":
-            self.network_param : BarlwoConfig = BarlwoConfig()  # TODO later we might need to do something
-            
+        if self.hparams.arch == "BarlowTwins":
+            self.network_param: BarlowConfig = (
+                BarlowConfig()
+            )  # TODO later we might need to do something
+            self.data_param: DatasetParams = DatasetParams()
+
         # Set random seed
         if self.hparams.seed_everything is None:
             self.hparams.seed_everything = random.randint(1, 10000)
+
         print("Random Seed: ", self.hparams.seed_everything)
         random.seed(self.hparams.seed_everything)
         torch.manual_seed(self.hparams.seed_everything)
